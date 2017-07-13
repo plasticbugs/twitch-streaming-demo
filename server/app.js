ae64b55ddf125411da80b53dd3defcdbb6f76767
Scott Moschella <scottmosch@gmail.com>
2017-07-13 11:00:57 -0700

    Add sessions and cookie parser for Google Sign-in

--- server/app.js
+++ server/app.js
@@ -1,7 +1,24 @@
 const express = require('express');
-const app = express();
 const router = require('../server/routes/main');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('../middleware/passport');
+const session = require('express-session');
+
+const app = express();
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  resave: true,
+  name: 'chatbox'
+}));
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', router);
 app.use(express.static(path.join(__dirname, '../public')));
