0e7f7015041c678abe7fd35effc37cdf48463476
Scott Moschella <scottmosch@gmail.com>
2017-07-13 14:33:44 -0700

    Create placeholder Live Video component

---
+++ app/components/LiveStreamVideo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LiveStreamVideo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="videostream">Video Stream Placeholder</div>;
+  }
+}
+
+module.exports = LiveStreamVideo;
