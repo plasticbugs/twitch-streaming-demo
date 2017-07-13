378ce721e55a184ece1fc1ec050e449cb1f0a103
Scott Moschella <scottmosch@gmail.com>
2017-07-13 14:25:19 -0700

    Handle login/logout and display search bar

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,14 +1,55 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UniversalCookies from 'universal-cookie';
+import axios from 'axios';
+import LoggedInUser from './LoggedInUser.jsx';
+import LoginScreen from './LoginScreen.jsx';
+import ChannelSearch from './ChannelSearch.jsx';
+
+const cookies = new UniversalCookies();
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      loggedIn: false,
+      currentUser: null
+    }
+    this.getCurrentUser = this.getCurrentUser.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch(e) {
+    console.log(e.target.value);
+  }
+
+  componentDidMount() {
+    if (cookies.get('loggedIn') === "true") {
+      this.setState({loggedIn: true});
+      this.getCurrentUser( data => {
+        this.setState({currentUser: data})
+      })
+    }
+  }
+
+  getCurrentUser(cb) {
+    axios.get('/api/getCurrentUser')
+    .then(response => {
+      console.log(response.data);
+     cb(response.data); 
+    });
   }
 
   render() {
+    let loginlogout;
+    console.log(this.state);
+    if(this.state.currentUser) {
+      loginlogout = <div><LoggedInUser userinfo={this.state.currentUser}/><ChannelSearch handleSearch={this.handleSearch} /></div>
+    } else {
+      loginlogout = <LoginScreen />;
+    }
     return (
-      <div>Hello World</div>
+      <div>{loginlogout}</div>
     )
   }
 }
