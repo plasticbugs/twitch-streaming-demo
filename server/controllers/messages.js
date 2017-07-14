577cf0865170d1f22236a42d578d223187853134
Scott Moschella <scottmosch@gmail.com>
2017-07-14 12:37:00 -0700

    Add logic for saving messages, show user messages

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,39 @@
 var User = require('../models/users');
+var Message = require('../models/messages');
 
 module.exports.saveMessage = (req, res) => {
-  console.log(req.body);
+  var message = new Message({
+    msgcontent: req.body.message
+  });
+  message.save(success => {
+    User.findOne({ username: req.body.user}, (err, user) => {
+      if(err) {
+        throw(err)
+      }
+      if(user) {
+        user.messages.push(message);
+        user.save();
+      } else {
+        user = new User({
+          username: req.body.user
+        });
+        user.messages.push(message);
+        user.save();
+      }
+    })
+  });
   res.sendStatus(200)
+}
+
+module.exports.showMessagesForUser = (req, res) => {
+  console.log(req)
+  User.findOne({ username: req.query.user})
+  .populate({ path: 'messages'})
+  .exec((err, result) => {
+    if(err) {
+      throw(err)
+    } else {
+      res.send(result);
+    }
+  })
 }
