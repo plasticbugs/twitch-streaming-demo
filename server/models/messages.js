aee10afec41f4e38c030e80c5d575607637b927f
Scott Moschella <scottmosch@gmail.com>
2017-07-14 12:38:11 -0700

    Rename content to msgcontent

--- server/models/messages.js
+++ server/models/messages.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var messageSchema = new Schema({
-  content: String
+  msgcontent: String
 });
 
 module.exports = mongoose.model('Message', messageSchema);
