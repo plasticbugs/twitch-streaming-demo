264a16eba9d136cfa840f9b158c45dc0884abc5b
Scott Moschella <scottmosch@gmail.com>
2017-07-14 12:34:55 -0700

    Clear search results and input field when channel selected

--- app/components/ChannelSearch.jsx
+++ app/components/ChannelSearch.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
+import SearchResults from './SearchResults.jsx';
 
 class ChannelSearch extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inputField: ''
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.clearCurrentSearch = this.clearCurrentSearch.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({inputField: e.target.value}, () => {
+      this.props.handleSearch(this.state.inputField);
+    })
+
+  }
+
+  clearCurrentSearch(e, clickedChannel) {
+    e.preventDefault();
+    this.setState({inputField: ''});
+    this.props.handleClick(clickedChannel);
   }
 
   render() {
-    return <div><input placeholder="Search for a channel" onInput={this.props.handleSearch} /></div>;
+    return (<div>
+      <input 
+        ref="channelSearch" 
+        placeholder="Search for a channel" 
+        onChange={this.handleInput}
+        value={this.state.inputField} 
+      />
+      <SearchResults 
+        handleChannelClick={this.clearCurrentSearch}
+        results={this.props.searchResults} 
+      />
+    </div>);
   }
 }
 
