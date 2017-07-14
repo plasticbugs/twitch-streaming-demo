ed7a07e74298030adea8cdfb839217a1813029fd
Scott Moschella <scottmosch@gmail.com>
2017-07-14 13:03:38 -0700

    Add logic for displaying user's message history

--- app/components/ChatWindow.jsx
+++ app/components/ChatWindow.jsx
@@ -6,34 +6,52 @@ class ChatWindow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showingUser: false,
+      showingUser: null,
       userhistory: null
     }
     this.handleUsernameClick = this.handleUsernameClick.bind(this);
+    this.goBackToChat = this.goBackToChat.bind(this);
   }
 
   handleUsernameClick(username) {
     console.log(username);
-    this.setState({showingUser: true}, () => {
-      axios.get('/api/user-messages', { params: {
-        user: username
-      }})
-      .then(response => {
-        this.setState({userhistory: response.data.messages})
+    axios.get('/api/user-messages', { params: {
+      user: username
+    }})
+    .then(response => {
+      this.setState({
+        showingUser: username,
+        userhistory: response.data.messages
       })
     })
+  }
+
+  goBackToChat(e) {
+    e.preventDefault();
+    this.setState({showingUser: null});
   }
 
   render() {
     let chatwindow;
     if (this.props.video) {
-      chatwindow = <div className="chatwindow">
-        { this.props.messages.map(item => {
-          return <div className="chatmessage" key={item.userstate.id}>
-            <a href="" onClick={(e) => {e.preventDefault();this.handleUsernameClick(item.userstate["display-name"])}} className="username">{item.userstate["display-name"]}</a>: {item.message}
-          </div>
-        })}
-      </div>
+      if (this.state.showingUser) {
+        chatwindow = <div className="chatwindow">Showing chat history for {this.state.showingUser}<br/>
+          <div className="goback"><a href="" onClick={this.goBackToChat}>Return to chat</a></div>
+          { this.state.userhistory.map(item => {
+            return <div className="chatmessage" key={item._id}>
+              {item.msgcontent}
+            </div>
+          })}
+        </div>
+      } else {
+        chatwindow = <div className="chatwindow">
+          { this.props.messages.map(item => {
+            return <div className="chatmessage" key={item.userstate.id}>
+              <a href="" onClick={(e) => {e.preventDefault();this.handleUsernameClick(item.userstate["display-name"])}} className="username">{item.userstate["display-name"]}</a>: {item.message}
+            </div>
+          })}
+        </div>
+      }
     }
     return (
       <div>{chatwindow}</div>
